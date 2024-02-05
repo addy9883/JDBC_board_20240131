24ad0fe1db48ed6a37ca53568d27de85fff04b83
addy9883 <addy9883@gmail.com>
2024-02-05 19:43:52 +0900

    게시물 작성시 DB에 게시물 정보가 저장되도록

--- src/main/java/com/woori/jdbc/board/App.java
+++ src/main/java/com/woori/jdbc/board/App.java
@@ -1,5 +1,9 @@
 package com.woori.jdbc.board;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,11 +31,53 @@ public class App {
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
-        String content = sc.nextLine();
+        String body = sc.nextLine();
 
         int id = ++articleLastId;
 
-        Article article = new Article(id, title, content);
+        String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=UTF-8";
+        String user = "root";
+        String password = "";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+          Class.forName(jdbcDriver);
+
+          conn = DriverManager.getConnection(url, user, password);
+
+          String sql = "INSERT INTO article";
+          sql += " SET regDate = NOW()";
+          sql += ", updateDate = NOW()";
+          sql += ", title = \"" + title + "\"";
+          sql += ", body = \"" + body + "\"";
+
+          ps = conn.prepareStatement(sql);
+          int affectedRows = ps.executeUpdate();
+
+          System.out.println("affectedRows : " + affectedRows);
+
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+        } catch (SQLException e) {
+          System.out.println("에러: " + e);
+        } finally {
+          try {
+            if (ps != null && !ps.isClosed()) {
+              ps.close();
+            }
+            if (conn != null && !conn.isClosed()) {
+              conn.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
+
+
+        Article article = new Article(id, title, body );
         articles.add(article);
 
         System.out.println("입력된 게시물 : " + article);
--- src/main/java/com/woori/jdbc/board/Article.java
+++ src/main/java/com/woori/jdbc/board/Article.java
@@ -3,13 +3,13 @@ package com.woori.jdbc.board;
 public class Article {
   public int id;
   public String title;
-  public String content;
+  public String body;
 
   //생성자 메서드 : 객체가 만들어질 때 딱 한번 실행된다.
-  public Article(int id,String title,String content){
+  public Article(int id,String title,String body){
     this.id = id;
     this.title = title;
-    this.content = content;
+    this.body = body;
   }
 
   @Override
@@ -17,7 +17,7 @@ public class Article {
     return "Article{" +
         "id=" + id +
         ", title='" + title + '\'' +
-        ", content='" + content + '\'' +
+        ", body='" + body + '\'' +
         '}';
   }
 }
